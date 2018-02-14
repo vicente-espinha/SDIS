ee90ed24bdec70ff966472c3f3c3ecb84c813d25
vicente-espinha <up201503764@fe.up.pt>
2018-02-14 12:43:19 +0000

    Mudanças para teste

--- Client.java
+++ Client.java
@@ -1,9 +1,28 @@
+import java.lang.String;
+import java.net.*;
+
+
 public class Client{
 
-  int main(){
+  private String host;
+  private String port;
+  private String oper;
+  private String opnd;
+
+  public void main(String[] args){
+
+    // if(args.lengt)
+    this.host = args[0];
+    this.port = args[1];
+    this.oper = args[2];
+    this.opnd = args[3];
+    //private DatagramSocket socket = new DatagramSocket(port.)
+    start();
 
   }
 
-  
+  public void start(){
+
+  }
 
 }
