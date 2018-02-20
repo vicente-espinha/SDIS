3b90ef1c8a2f6c81c8e8587d3882c23c2a2239b5
João Seixas <up201505648@fe.up.pt>
2018-02-20 19:26:13 +0000

    errors

--- Client.java
+++ Client.java
@@ -4,20 +4,44 @@ import java.net.*;
 
 public class Client{
 
-  private String host;
-  private String port;
-  private String oper;
-  private String opnd;
-
   public void main(String[] args){
+    if (args.length != 4 || args.length != 3) {
+      System.out.println("Usage: java Client <hostname> <port> <operation> <operand>+");
+      return;
+    }
 
-    // if(args.lengt)
-    this.host = args[0];
-    this.port = args[1];
-    this.oper = args[2];
-    this.opnd = args[3];
-    //private DatagramSocket socket = new DatagramSocket(port.)
-    start();
+    String host = args[0];
+    String port = args[1];
+    String oper = args[2];
+    String opnd = "";
+    if(args.length == 4)
+       opnd = args[3];
+    DatagramSocket socket = new DatagramSocket();
+    String request = oper + ":" + opnd;
+    byte[] sbuf = request.getBytes();
+    InetAddress address = InetAddress.getByName(host);
+    DatagramPacket packet = new DatagramPacket(sbuf, sbuf.length, address, port);
+    socket.send(packet);
+
+    //get response
+    byte[] rbuf = new byte[sbuf.length];
+    packet = new DatagramPacket(rbuf, rbuf.length);
+    socket.receive(packet);
+
+    // display response
+    String received = new String(packet.getData());
+    if(received.contains(";")){
+      String[] parts = received.split(";");
+      System.out.println(parts[0] + " belongs to " + parts[1]);
+
+    } else {
+      if(received == -1)
+        System.out.println("Error in request");
+      else
+        System.out.println("Registed with success. " + received + " vehicles in database");
+    }
+    socket.close();
+        start();
 
   }
 
--- Server.java
+++ Server.java
@@ -1,5 +1,5 @@
 import java.lang.String;
-
+import java.net.*;
 
 public class Server{
 
@@ -7,10 +7,47 @@ public class Server{
   public static void main(String[] args){
 
     if(args.length == 1){
-
+      String cars = "";
       String port = args[0];
-      System.out.println(port);
+      System.out.println("Opened server in port " + port);
+      DatagramSocket socket = new DatagramSocket();
+
+      byte[] buf = new byte[256];
+      DatagramPacket packet = new DatagramPacket(buf, buf.length);
+      socket.receive(packet);
+
+      String[] msg = (new String(packet.getData())).split(":");
+      String answer;
+      if(msg[0] == "register"){
+        answer = register(cars, msg[1], msg[2]);
+
+      } else if(msg[0] == "lookup") {
+        answer = lookup(cars, msg[1]);
+      } else {
+        answer = "-1";
+      }
+
+      InetAddress address = packet.getAddress();
+      packet = new DatagramPacket(answer, answer.length, address, port);
+      socket.send(packet);
     }
+  }
+
+  public String register(String cars, String plate, String owner){
+    if(cars.length() != 0)
+      cars += "%";
+    cars += plate + ";" + owner;
+    return cars.split("%").length();
+  }
+
+  public String lookup(String cars, String plate){
+    int idx = cars.indexOf(plate);
+    if(idx > -1){
+      int idx2 = cars.indexOf("%", idx);
+      String car = cars.substring(idx, idx2);
+      return car;
+    }
+    return "-1";
   }
 
 
