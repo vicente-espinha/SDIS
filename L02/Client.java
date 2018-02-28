11d8abdbb0aaa39633b4ecb228f9a8e38c3548e8
João Seixas <up201505648@fe.up.pt>
2018-02-28 11:51:45 +0000

    Starting L02

---
+++ L02/Client.java
@@ -0,0 +1,86 @@
+import java.lang.String;
+import java.net.*;
+import java.lang.Integer;
+import java.io.IOException;
+
+
+public class Client{
+
+  private DatagramSocket socket;
+
+  public static void main(String[] args)throws IOException{
+
+    if (args.length != 5 && args.length != 4) {
+      System.out.println("Usage: java Client <hostname> <port> <operation> <operand>+");
+      return;
+    }
+    Client client = new Client();
+
+    String mcast_addr = args[1];
+    String mcast_port = args[2];
+    String oper = args[2];
+    String plate = args[3];
+    InetAddress address = InetAddress.getByName(host);
+    String request = new String();
+
+
+    if(oper.equals("REGISTER")){
+      String owner = args[4];
+      request = oper + ";" + plate + ";" + owner;
+    }else if(oper.equals("LOOKUP")){
+      request = oper + ";" + plate;
+    }else{
+      System.out.println("Operation invalid !");
+      System.exit(-1);
+    }
+
+    //sending message
+    byte[] sbuf = request.getBytes();
+    DatagramPacket packet = new DatagramPacket(sbuf, sbuf.length, address, Integer.parseInt(port));
+    client.sendPacket(packet);
+
+    //get response
+    byte[] rbuf = new byte[512];
+    DatagramPacket packetreceived = new DatagramPacket(rbuf, rbuf.length);
+    client.receivePacket(packetreceived);
+
+    // display response
+    String received = new String(packetreceived.getData());
+    if(received.contains(";")){
+      String[] parts = received.split(";");
+      System.out.println(parts[0] + " belongs to " + parts[1]);
+
+    } else {
+      if(received.equals("-1"))
+        System.out.println("Error in request");
+      else
+        System.out.println("Registed with success. " + received + " vehicles in database");
+    }
+    client.socket.close();
+
+  }
+
+  public Client(){
+    try{
+      this.socket = new DatagramSocket();
+    }catch(SocketException e){
+      System.out.println("Error opening socket!\n");
+    }
+  }
+
+  public void sendPacket(DatagramPacket packet) throws IOException{
+    try{
+      this.socket.send(packet);
+    }catch(SocketException e){
+      System.out.println("Error sending packet to socket!\n");
+    }
+  }
+
+  public void receivePacket(DatagramPacket packet)throws IOException{
+    try{
+      this.socket.receive(packet);
+    }catch(SocketException e){
+      System.out.println("Error sending packet to socket!\n");
+    }
+  }
+}
---
+++ L02/Server.java
@@ -0,0 +1,100 @@
+import java.lang.String;
+import java.net.*;
+import java.lang.Integer;
+import java.io.IOException;
+import java.util.Hashtable;
+
+public class Server{
+
+  private DatagramSocket socket;
+  private Hashtable<String,String> dataBase;
+
+  public static void main(String[] args) throws IOException{
+
+    if(args.length == 3){
+
+      String port = args[0];
+      String mcast_addr = args[1];
+      String mcast_port = args[2];
+      Server server = new Server(port);
+      System.out.println("Opened server in port " + port);
+
+      while(true){
+        byte[] buf = new byte[512];
+        DatagramPacket packet = new DatagramPacket(buf,512);
+
+        server.receivePacket(packet);
+        String[] msg = (new String(packet.getData())).split(";");
+
+        String answer = new String();
+        System.out.println(msg[0] + " + " + msg[1]);
+        if(msg[0] == "REGISTER"){
+          answer = server.register(msg[1], msg[2]); //processes register
+        } else if(msg[0].equals("LOOKUP")) {
+          answer = server.lookup(msg[1]);  //processes lookup
+        } else {
+          answer = "-1";
+        }
+
+        DatagramPacket packetsend = new DatagramPacket(answer.getBytes(), answer.getBytes().length, packet.getAddress(), packet.getPort());  //creates packet
+        server.sendPacket(packetsend); //sends packet
+
+      }
+    }
+  }
+
+  public Server(String port){
+    try{
+      this.socket = new DatagramSocket(Integer.parseInt(port));
+      this.dataBase = new Hashtable<>();
+    }catch(SocketException e){
+      System.out.println("Error opening socket!\n");
+    }
+  }
+
+  public void sendPacket(DatagramPacket packet) throws IOException{
+    try{
+      this.socket.send(packet);
+    }catch(SocketException e){
+      System.out.println("Error sending packet to socket!\n");
+    }
+  }
+
+  public void receivePacket(DatagramPacket packet)throws IOException{
+    try{
+      this.socket.receive(packet);
+    }catch(SocketException e){
+      System.out.println("Error sending packet to socket!\n");
+    }
+  }
+
+  public String register(String plate, String owner){
+
+    System.out.println("PLATE: " + plate + "  " + "Owner:" + owner);
+
+      if(this.dataBase.get(plate) == null){  //checks if the plate already exists in dataBase
+        this.dataBase.put(plate,owner);
+        return Integer.toString(this.dataBase.size());          //return the size od the dataBase
+      }
+      else{
+        System.out.println("Already Registered!");
+        return "-1";
+      }
+  }
+
+  public String lookup(String plate){
+
+    System.out.println("PLATE: " + plate);
+
+    if(this.dataBase.get(plate) == null){  //checks if the plate already exists in dataBase
+      System.out.println("Owner not Found!");
+      return "NOT_FOUND";                   //returns not found if the plate does not exist in the dataBase
+    }
+    else{
+      return this.dataBase.get(plate);          //return the owner
+    }
+
+  }
+
+
+}
