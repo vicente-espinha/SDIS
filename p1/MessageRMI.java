b3d691086a4d5f8e62d628e906328dfc31e9c1f1
João Seixas <up201505648@fe.up.pt>
2018-03-26 08:53:46 +0100

    Cleaned code and now can receive more than 1 msg

--- p1/MessageRMI.java
+++ p1/MessageRMI.java
@@ -3,9 +3,9 @@ import java.rmi.RemoteException;
 import java.io.IOException;
 
 public interface MessageRMI extends Remote {
-    String backup() throws RemoteException, IOException;;
-    String restore() throws RemoteException;
-    String delete() throws RemoteException;
-    String manage() throws RemoteException;
-    String retrieve() throws RemoteException;
+    String backup() throws RemoteException, IOException;
+    String restore() throws RemoteException, IOException;
+    String delete() throws RemoteException, IOException;
+    String manage() throws RemoteException, IOException;
+    String retrieve() throws RemoteException, IOException;
 }
--- p1/Peer.java
+++ p1/Peer.java
@@ -7,51 +7,48 @@ import java.lang.String;
 import java.io.*;
 
 public class Peer implements MessageRMI {
-    private InetAddress group;
-    private MulticastSocket msocket;
+    private MC mc; 
 
     public Peer(String[] args) throws IOException{
-        try{
-            this.group = InetAddress.getByName(args[3]);
-            this.msocket = new MulticastSocket(4446);
-          System.out.println(args[3]);
-        }catch(SocketException e){
-          System.out.println("Error opening socket!\n");
-        }
-        msocket.joinGroup(group);
+        
+       this.mc = new MC(args[3], args[4]);
     }
 
-    private void init(String[] args) throws IOException {
+    public void getMessage() throws IOException {
 
-
-
-        byte[] buf = new byte[1000];
-        DatagramPacket recv = new DatagramPacket(buf, buf.length);
-        this.msocket.receive(recv);
-        System.out.println(buf);
+        this.mc.receiveMessage();
     }
 
     public String backup() throws IOException {
+
         String msg = "Backup a file";
-        DatagramPacket backup = new DatagramPacket(msg.getBytes(), msg.length(),
-                            this.group, 4446);
-        this.msocket.send(backup);
+        this.mc.sendMessage(msg);
         return "Backed a file";
     }
 
-    public String restore() {
+    public String restore() throws IOException {
+
+        String msg = "Restored a file";
+        this.mc.sendMessage(msg);
         return "Restore a file";
     }
 
-    public String delete() {
+    public String delete() throws IOException {
+
+        String msg = "Deleted a file";
+        this.mc.sendMessage(msg);
         return "Delete a file";
     }
 
-    public String manage() {
+    public String manage() throws IOException {
+        String msg = "Managed a file";
+        this.mc.sendMessage(msg);
         return "Manage a file";
     }
 
-    public String retrieve() {
+    public String retrieve() throws IOException {
+        String msg = "Retrieved a file";
+        this.mc.sendMessage(msg);
         return "Retrieve a file";
     }
 
@@ -60,26 +57,24 @@ public class Peer implements MessageRMI {
             System.out.println("Number of arguments not correct..");
             return;
         }
-        //Peer thisPeer = new Peer(args[3]);
+
         Peer thisPeer = new Peer(args);
 
+            try {
 
+                MessageRMI stub = (MessageRMI) UnicastRemoteObject.exportObject(thisPeer, 0);
 
-                try {
+                // Bind the remote object's stub in the registry
+                Registry registry = LocateRegistry.getRegistry();
+                registry.bind(args[1], stub);
 
-                    MessageRMI stub = (MessageRMI) UnicastRemoteObject.exportObject(thisPeer, 0);
+                System.out.println("Peer ready");
+            } catch (Exception e) {
+                System.err.println("Peer exception: " + e.toString());
+                e.printStackTrace();
+            }
 
-                    // Bind the remote object's stub in the registry
-                    Registry registry = LocateRegistry.getRegistry();
-                    registry.bind(args[1], stub);
-
-                    System.out.println("Peer ready");
-                } catch (Exception e) {
-                    System.err.println("Peer exception: " + e.toString());
-                    e.printStackTrace();
-                }
-
-        thisPeer.init(args);
+        thisPeer.getMessage();
 
     }
 }
