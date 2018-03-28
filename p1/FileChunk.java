a17ad209ffe84d9f1692f278bb6882c45b4f8f8b
Vicente Espinha <up201503764@fe.up.pt>
2018-03-28 12:21:07 +0100

    saves chunck localy

--- p1/FileChunk.java
+++ p1/FileChunk.java
@@ -1,33 +1,40 @@
 
+public class FileChunk {
 
-public class FileChunk{
-    
     byte[] fileID;
     int chunkNumber;
     byte[] body;
     int repDegree;
 
-    public FileChunk(byte[] fileID, int chunkNumber, byte[] body, int repDegree){
+    public FileChunk(byte[] fileID, int chunkNumber, byte[] body, int repDegree) {
         this.fileID = fileID;
         this.chunkNumber = chunkNumber;
         this.body = body;
         this.repDegree = repDegree;
     }
 
-    public byte[] getBody(){
+    public byte[] getBody() {
         return this.body;
     }
 
-    public int getNumber(){
+    public int getNumber() {
         return this.chunkNumber;
     }
 
-    public byte[] getFileID(){
+    public byte[] getFileID() {
         return this.fileID;
     }
 
-    public int getRepDeg(){
+    public int getRepDeg() {
         return this.repDegree;
+    }
+
+    public void save(String peerID) {
+
+        //opens file with filename = fileID + chunknumber
+        FileOutputStream out = new FileOutputStream(peerID + String(this.fileID) + Integer.toString(this.chunkNumber));
+        out.write(this.body); //writes in bynary file
+        out.close(); //closes output stream
     }
 
 }
